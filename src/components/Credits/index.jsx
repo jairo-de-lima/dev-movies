4ac2a8f352562f7b851ff140fd8eceaeff5c8c0a
jairo-de-lima <jairodelima94@gmail.com>
2024-07-18 18:47:00 -0300

    Projeto Dev-Movies

---
+++ src/components/Credits/index.jsx
@@ -0,0 +1,22 @@
+import { Container, Title } from "./style";
+import { getImages } from "../../../utils/getImages";
+
+function Credits({ credits }) {
+  return (
+    <>
+      <Title>Creditos</Title>
+      {credits && (
+        <Container>
+          {credits.slice(0, 5).map((artist) => (
+            <div key={artist.cast_id}>
+              <img src={getImages(artist.profile_path)} alt={artist.name} />
+              <p>{artist.original_name}</p>
+            </div>
+          ))}
+        </Container>
+      )}
+    </>
+  );
+}
+
+export default Credits;
---
+++ src/components/Slider/style.js
@@ -0,0 +1,19 @@
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+    padding: 0 20px;
+    overflow: hidden;
+    background: radial-gradient(circle, rgba(134,131,132,1) 23%, rgba(1,13,28,1) 75%);
+
+    h2 {
+        font-size: 28px;
+        color: #fff;
+        margin: 50px 0 20px 20px;
+    }
+
+  .swiper-wrapper {
+    display: flex;
+    gap: 10px;
+  }
+  
+`
